8bcf8c33666196c7e8e3dd1b25efbac36cca9fbe
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-09-26 12:57:08 -0400

    purrrr

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -6,7 +6,6 @@
     of the program.
 */
 
-
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
@@ -14,7 +13,12 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 10)
+const size = parseInt(process.argv[2], 10);
+
+if (isNaN(size) || size < 1) {
+    process.stderr.write("Invalid size\n");
+    process.exit(1);
+}
 
 for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
